9dbe3ba03d6d3567160f3a4087590df38234f25b
marygong75 <43527288+marygong75@users.noreply.github.com>
2019-01-08 21:38:07 -0600

    Add files via upload

--- MaryGhomework.sql
+++ MaryGhomework.sql
@@ -1,6 +1,7 @@
 -- 1a. Display the first and last names of all actors from the table actor.
 USE sakila;
-SELECT *FROM actor;
+SELECT first_name, last_name
+FROM actor;
 
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 ALTER TABLE actor ADD COLUMN Actor_name VARCHAR(100);
